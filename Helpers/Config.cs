d0511cf1071335ed1fcf49ded45fa3bc3f21db66
Kimberly Spiegel <kspiegel72@mac.mynetworksettings.com>
2025-08-03 12:59:06 -0400

    comment formatting

--- Helpers/Config.cs
+++ Helpers/Config.cs
@@ -1,7 +1,9 @@
 namespace TestProject.Helpers
 {
-    // Defines the root directory of the file system accessible to the application
-    // This can be set at runtime (e.g. dotnet run /Users/jsmith/Documents/Test/)
+    /// <summary>
+    /// Defines the root directory of the file system accessible to the application
+    /// This can be set at runtime (e.g. dotnet run /Users/jsmith/Documents/Test/)
+    /// </summary>
     public static class Config
     {
         public static string RootDirectory { get; set; } = "./";
