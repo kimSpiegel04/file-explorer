648316e363596de4421d067398f672fcc45d7c5b
Kimberly Spiegel <kspiegel72@mac.mynetworksettings.com>
2025-08-03 12:28:19 -0400

    config class doc

--- Helpers/Config.cs
+++ Helpers/Config.cs
@@ -1,5 +1,7 @@
 namespace TestProject.Helpers
 {
+    // Defines the root directory of the file system accessible to the application
+    // This can be set at runtime
     public static class Config
     {
         public static string RootDirectory { get; set; } = "./";
